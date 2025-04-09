8b09a1b0888530568581ebd028dde32502ac9773
Guilherme <gulherme.marret@g.globo>
2025-04-09 20:43:58 -0300

    Added this scheduler call to side thread

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
+#include "thread.h"
 
 int main(void){
-    printf("Hello, World!\n");
+    start_scheduler_thread();
+
+    while(1){
+        printf("Main program is running");
+        sleep(20);
+    }
+
     return 0;
 }
---
+++ thread.c
@@ -0,0 +1,20 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "scheduler.h"
+
+void *thread_function(void *arg) {
+    scheduler();
+    return NULL;
+}
+
+void start_scheduler_thread() {
+    pthread_t thread_id;
+
+    if (pthread_create(&thread_id, NULL, thread_function, NULL) != 0) {
+        perror("Failed to create thread");
+        exit(EXIT_FAILURE);
+    }
+    // Getting warn just with the thread_id and the makefile Werror flag dont compile irra
+    printf("Scheduler thread started with ID: %ld\n", (unsigned long)thread_id);
+}
