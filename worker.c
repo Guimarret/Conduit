358a362a039e318b1163ebd33f131a0ceadc80f6
Guimarret <guimarret@gmail.com>
2025-04-29 20:42:56 -0300

    idea too the worker

---
+++ worker.c
@@ -0,0 +1,2 @@
+// Thinking about creating binarys files that are executed here but idk how should i pass the other params needed in the scheduler
+// Not sure if is the best way but looks funny to implement
