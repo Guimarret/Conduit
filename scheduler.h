7c1d09fcdde361087117dc399626eae05c45e305
Guimarret <guimarret@gmail.com>
2025-05-03 14:35:24 -0300

    these headers was looping and getting errors so def!

--- scheduler.h
+++ scheduler.h
@@ -1,3 +1,6 @@
+#ifndef CONDUIT_SCHEDULER_H
+#define CONDUIT_SCHEDULER_H
+
 typedef struct Task{
     char taskName[64];
     char cronExpression[256];
@@ -6,3 +9,7 @@ typedef struct Task{
 } Task;
 
 void scheduler(void);
+Task* add_task(const char *name, const char *cronExpression, const char *execution);
+void free_tasks(void);
+
+#endif
--- thread.h
+++ thread.h
@@ -1,9 +1,17 @@
-void start_scheduler_thread();
-void spawn_worker_thread();
-void spawn_worker_thread(struct Task *task);
+#ifndef CONDUIT_THREAD_H
+#define CONDUIT_THREAD_H
 
-typedef struct {
+// Forward declaration instead of including scheduler.h
+struct Task;  // Just declare the struct without defining it
+typedef struct Task Task;  // Define the typedef
+
+void start_scheduler_thread(void);
+void spawn_worker_thread(Task *task);
+
+typedef struct ThreadParams{
     int taskId;
     char taskName[64];
     char taskExecution[64];
 } ThreadParams;
+
+#endif
