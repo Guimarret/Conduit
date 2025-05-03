9431ab918b871b4730b45ef6ab9b1fa41b72233c
Guimarret <guimarret@gmail.com>
2025-05-02 22:16:43 -0300

    heheh

---
+++ hash.c
@@ -0,0 +1,8 @@
+unsigned long hashString(const char *str) {
+    unsigned long hash = 5381;
+    int c;
+    while ((c = *str++)) {
+        hash = ((hash << 5) + hash) + c;
+    }
+    return hash;
+}
