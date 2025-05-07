59501a7cb016fabad6f8b64bc4a0e1efcf192969
Guimarret <guimarret@gmail.com>
2025-05-07 20:23:45 -0300

    Added dag_status call in database funcs

--- database.c
+++ database.c
@@ -1,6 +1,8 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sqlite3.h>
+#include <string.h>
 #include "scheduler.h"
 
 sqlite3* initialize_database() {
@@ -81,4 +83,53 @@ sqlite3* dag_import(sqlite3 *db, Task *task){
         sqlite3_finalize(stmt);
     }
     return db;
+}
+
+char* dags_status(sqlite3 *db){
+    const char *sql = "SELECT id, taskName, cronExpression, taskExecution FROM tasks";
+    sqlite3_stmt *stmt;
+    char *json_result = NULL;
+    size_t json_size = 0;
+    size_t json_capacity = 1024;
+
+    json_result = malloc(json_capacity);
+    if (!json_result) return NULL;
+
+    strcpy(json_result, "{\"tasks\": [");
+    json_size = strlen(json_result);
+
+
+    int rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+    if (rc != SQLITE_OK){
+        fprintf(stderr, "Database error: %s\n", sqlite3_errmsg(db));
+        free(json_result);
+        return NULL;
+    }
+
+    int first_row = 1;
+    while((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
+        int id = sqlite3_column_int(stmt, 0);
+        const char *name = (const char*)sqlite3_column_text(stmt, 1);
+        const char *cron = (const char*)sqlite3_column_text(stmt, 2);
+        const char *exec = (const char*)sqlite3_column_text(stmt, 3);
+
+        char row_buffer[512];
+        snprintf(row_buffer, sizeof(row_buffer),
+                         "%s{\"id\":%d,\"name\":\"%s\",\"schedule\":\"%s\",\"execution\":\"%s\"}",
+                         first_row ? "" : ",", id, name, cron, exec);
+        first_row = 0;
+        if(json_size + strlen(row_buffer) >= json_capacity){
+            json_capacity *= 2;
+            json_result = realloc(json_result, json_capacity);
+            if (!json_result) return NULL;
+        }
+
+        strcat(json_result, row_buffer);
+        json_size += strlen(row_buffer);
+    }
+
+    strcat(json_result, "}]");
+    sqlite3_finalize(stmt);
+
+    return json_result;
 }
--- database.h
+++ database.h
@@ -5,3 +5,4 @@ sqlite3* initialize_database();
 void shutdown_database(sqlite3 *db);
 sqlite3* dag_migration(sqlite3 *db);
 sqlite3* dag_import(sqlite3 *db, Task *task);
+char* dags_status(sqlite3 *db);
