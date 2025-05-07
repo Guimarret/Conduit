e80002bc97b2335e626f9cfa8a51661aa07a7699
Guimarret <guimarret@gmail.com>
2025-05-07 17:55:34 -0300

    Changed the path of the webserver, so in the future its gonna be easy to separate in multiple cluster if needed

--- webserver.c
+++ webserver.c
@@ -2,15 +2,19 @@
 #include <stdio.h>
 
 static int home_handler(struct mg_connection *conn, void *cbdata) {
-    mg_send_file(conn, "./webroot/home.html");
-    return 1; // Handled
+    mg_send_file(conn, "./webroot/html/home.html");
+    return 1;
+}
+
+static int dash_handler(struct mg_connection *conn, void *cbdata) {
+    mg_send_file(conn, "./webroot/html/dash.html");
+    return 1;
 }
 
 int main(void) {
     struct mg_context *ctx;
     mg_init_library(0);
 
-    // Server configuration
     const char *options[] = {
         "listening_ports", "8080",
         "document_root", "./webroot", // Path to static files
@@ -20,6 +24,7 @@ int main(void) {
     ctx = mg_start(NULL, 0, options);
 
     mg_set_request_handler(ctx, "/home", home_handler, NULL);
+    mg_set_request_handler(ctx, "/dash", dash_handler, NULL);
 
     printf("Server running at http://localhost:8080. Press Enter to quit.\n");
     getchar();
