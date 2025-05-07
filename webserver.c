bbb2069a62147af70d7fddf782d2497d92c54283
Guimarret <guimarret@gmail.com>
2025-05-06 21:15:12 -0300

    Ultra basic website, tested some features of this lib and looks good

--- webserver.c
+++ webserver.c
@@ -1,2 +1,30 @@
-// Implement using https://github.com/lammertb/libhttp
-// Also enable javascript
+#include "civetweb/civetweb.h"
+#include <stdio.h>
+
+static int home_handler(struct mg_connection *conn, void *cbdata) {
+    mg_send_file(conn, "./webroot/home.html");
+    return 1; // Handled
+}
+
+int main(void) {
+    struct mg_context *ctx;
+    mg_init_library(0);
+
+    // Server configuration
+    const char *options[] = {
+        "listening_ports", "8080",
+        "document_root", "./webroot", // Path to static files
+        "enable_directory_listing", "no", // Optional
+        NULL
+    };
+    ctx = mg_start(NULL, 0, options);
+
+    mg_set_request_handler(ctx, "/home", home_handler, NULL);
+
+    printf("Server running at http://localhost:8080. Press Enter to quit.\n");
+    getchar();
+
+    mg_stop(ctx);
+    mg_exit_library();
+    return 0;
+}
