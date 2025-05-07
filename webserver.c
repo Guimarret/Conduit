b654907e5f56672d5b4c952903d6c52dd1698237
Guimarret <guimarret@gmail.com>
2025-05-07 20:57:59 -0300

    fixed the api handling and put some better output in error case because othewise there is no debug...

--- webserver.c
+++ webserver.c
@@ -1,17 +1,37 @@
 #include <stdio.h>
 #include <string.h>
-#include <time.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "civetweb/civetweb.h"
 #include "database.h"
 
 static int api_data_handler(struct mg_connection *conn, void *cbdata) {
     sqlite3 *db = (sqlite3 *)cbdata;
     char *json_data = dags_status(db);
+    if (!json_data) {
+        // Send empty response if there's an error
+        const char *empty_json = "{\"tasks\":[]}";
+        mg_printf(conn, "HTTP/1.1 200 OK\r\n"
+                      "Content-Type: application/json\r\n"
+                      "Access-Control-Allow-Origin: *\r\n"
+                      "Content-Length: 11\r\n"
+                      "\r\n%s", empty_json);
+        return 1;
+    }
 
-    mg_send_http_ok(conn, "application/json; charset=utf-8", -1);
-    mg_write(conn, json_data, strlen(json_data));
+    size_t data_len = strlen(json_data);
 
+    mg_printf(conn, "HTTP/1.1 200 OK\r\n"
+                   "Content-Type: application/json\r\n"
+                   "Access-Control-Allow-Origin: *\r\n"
+                   "Content-Length: %lu\r\n"
+                   "\r\n", (unsigned long)data_len);
+
+
+    mg_write(conn, json_data, data_len);
+
+    free(json_data);
     return 1;
 }
 
@@ -25,7 +45,7 @@ int initialize_webserver(sqlite3 *db) {
         NULL
     };
     ctx = mg_start(NULL, 0, options);
-    mg_set_request_handler(ctx, "/api/dag_data", api_data_handler, NULL);
+    mg_set_request_handler(ctx, "/api/dag_data", api_data_handler, db);
 
     printf("Server running at http://localhost:8080.\n");
     getchar();
