5ecd79626e0696f2b235b0f68d5363c8653d35be
Guimarret <guimarret@gmail.com>
2025-05-13 13:58:27 -0300

    Insert new tasks function

--- database.c
+++ database.c
@@ -37,6 +37,20 @@ sqlite3* dag_migration(sqlite3 *db){
     log_message("Migration executed");
     return db;
 }
+sqlite3* insert_new_dag(sqlite3 *db, Task *task){
+    const char *sql;
+    sqlite3_stmt *stmt;
+
+    sql = "INSERT INTO tasks (taskName, cronExpression, taskExecution) VALUES (?, ?, ?)";
+    sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+    sqlite3_bind_text(stmt, 1, task->taskName, -1, SQLITE_TRANSIENT);
+    sqlite3_bind_text(stmt, 2, task->cronExpression, -1, SQLITE_TRANSIENT);
+    sqlite3_bind_text(stmt, 3, task->taskExecution, -1, SQLITE_TRANSIENT);
+    sqlite3_step(stmt);
+    sqlite3_finalize(stmt);
+
+    return db;
+};
 
 // Change to overwrite cause its creating new in every run
 sqlite3* insert_into_db(sqlite3 *db, Task *task){
