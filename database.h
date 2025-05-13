d3484f0c53684c5a7ff9f1118b77e64da9587bfa
Guimarret <guimarret@gmail.com>
2025-05-13 13:58:35 -0300

    Update database.h

--- database.h
+++ database.h
@@ -6,3 +6,4 @@ void shutdown_database(sqlite3 *db);
 sqlite3* dag_migration(sqlite3 *db);
 sqlite3* dag_import(sqlite3 *db, Task *task);
 char* dags_status(sqlite3 *db);
+sqlite3* insert_new_dag(sqlite3 *db, Task *task);
